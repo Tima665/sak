5e7822619815abc7306758154bc544ae09a517c5
Timur Vorobev <timurvorobyov@Timurs-MacBook-Pro.local>
2025-06-11 01:03:37 +0500

    Update Capacitor app ID from 'com.saktimur.app' to 'com.timur.sak' in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.saktimur.app',
+  appId: 'com.timur.sak',
   appName: 'sak',
   webDir: 'dist',
 };
