b505cb5b8d19987745076ddcd337b6bf5de9bf1a
Timur Vorobev <timurvorobyov@Timurs-MacBook-Pro.local>
2025-06-11 02:47:32 +0500

    Update Vite configuration: add build options for ESNext target, optimize dependencies for better performance, and enhance esbuild settings for improved compatibility.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,33 @@
 import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), legacy()],
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        format: 'es',
+      },
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+      supported: {
+        bigint: true,
+      },
+    },
+  },
+  esbuild: {
+    target: 'esnext',
+    supported: {
+      bigint: true,
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
