77f15231da8e83a30f3b7e3faa079e114e34a32e
Timur Vorobev <timurvorobyov@Timurs-MacBook-Pro.local>
2025-06-12 03:06:48 +0500

    Update package dependencies: add crypto-browserify, process, and stream-browserify; enhance Vite configuration with polyfills for Node globals and update polyfills.ts to include process support.

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,20 @@ import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import path from 'path';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), legacy()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+      process: 'process/browser',
+    },
+  },
   build: {
     target: 'esnext',
     rollupOptions: {
@@ -18,6 +28,13 @@ export default defineConfig({
   },
   optimizeDeps: {
     esbuildOptions: {
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
       target: 'esnext',
       supported: {
         bigint: true,
